dd4d94bdab577b33b37fa675baa6e493ec2359a9
Tolulope Fakoya <toluhunter@Toluhunter.localdomain>
2020-10-02 07:32:14 +0100

    Assignment done

--- Main.java
+++ Main.java
@@ -2,45 +2,45 @@ class Main {
   public static void main(String[] args) {
    // kindly use appropriate data types for your declaration
    //declare an array variable and assign the days of the month debtor defaulted,
-   byte days_defaulted[]={1,17,19,24,30};
+   byte daysDefaulted[]={1,17,19,24,30};
    // declare variables of amount the debtor is to pay per day, and amount the debtor paid and assign values to them
-   final byte amount_perday=118;
+   final byte amountPerday=118;
    final float paid=1800;
   
    // declare all the variables needed for your calculations
-    float to_pay=0f,total_amount=0f,extra_amount,left_topay;
-    byte days_paid,days_notpaid;
+    float toPay=0f,totalAmount=0f,extraAmount,leftTopay;
+    byte daysPaid,daysNotPaid;
     byte i=0;//for iteration
    
    // calculate and print total amount the debtor is to pay using for each loop
-    for(byte x : days_defaulted)
-    total_amount+=amount_perday;
-    System.out.println("Total amount to pay is "+total_amount);
+    for(byte x : daysDefaulted)
+    totalAmount+=amountPerday;
+    System.out.println("Total amount to pay is "+totalAmount);
    // calculate the days the debtor paid for and print the days in which was paid for starting from the left to the right of the array. Also check if there was a day that the debtor did not pay the complete amount for that day, if yes, print the day and the amount left to pay for that day. 
-    days_paid=(int)(paid/amount_perday);
+    daysPaid=(int)(paid/amountPerday);
     System.out.print("days debtor paid for ");
-    for(i=0;i<5 &&i<days_paid;i++){
-      System.out.print(days_defaulted[i]+",");
+    for(i=0;i<5 &&i<daysPaid;i++){
+      System.out.print(daysDefaulted[i]+",");
     }
-    if(days_paid==0)
+    if(daysPaid==0)
     System.out.print("None");
     System.out.println("");
-    if (paid%amount_perday>0&&i<5 ){
-      left_topay=amount_perday-(paid%amount_perday);
-    System.out.println("You did not complete day "+days_defaulted[i]+ " amount left to pay is "+left_topay);
+    if (paid%amountPerday>0&&i<5 ){
+      leftTopay=amountPerday-(paid%amountPerday);
+    System.out.println("You did not complete day "+daysDefaulted[i]+ " amount left to pay is "+leftTopay);
     }
    // calculate amount the debtor is left to  pay if he did not pay complete fees and the days not paid for or  check if the debtor paid extra and the extra amount he paid for, or if he paid the exact amount needed to be paid and print likewise
-   if(total_amount>paid){
-     to_pay=total_amount-paid;
-     days_notpaid=(5-(int)(paid/amount_perday));
-   System.out.println("Amount left to pay is "+to_pay +" you did not pay for "+days_notpaid+" day(s)");
+   if(totalAmount>paid){
+     toPay=totalAmount-paid;
+     daysNotPaid=(5-(int)(paid/amountPerday));
+   System.out.println("Amount left to pay is "+toPay +" you did not pay for "+daysNotPaid+" day(s)");
    }
-   else if(total_amount==paid){
+   else if(totalAmount==paid){
      System.out.println("You paid the exact amount");
    }
    else{
-     extra_amount=paid-total_amount;
-     System.out.println("You paid extra by "+extra_amount);
+     extraAmount=paid-totalAmount;
+     System.out.println("You paid extra by "+extraAmount);
    }
   }
 }
